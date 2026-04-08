a3aed57a35fc9024d670bb23001288247a141f12
vitorPatrick <vitor_patrick@yahoo.com>
2026-04-07 22:01:41 -0300

    adaad

---
+++ atv/FormasGeometricas.java
@@ -0,0 +1,13 @@
+package com.mycompany.calculararea;
+public class FormasGeometricas {
+    
+    public FormasGeometricas(){
+    }
+    
+    public double calcularArea(){
+        return 0;
+    }
+    public double calcularPerimetro(){
+        return 0;
+    }
+}
